f10c18cd247d149a3cd29b3e2325f715451d1b62
Vadim <75739522+vdsmirnov@users.noreply.github.com>
2021-09-26 00:46:37 +0700

    Add files via upload

---
+++ lab 5 task 1-5/task2.cpp
@@ -0,0 +1,17 @@
+﻿// Даны три точки A, B, C на числовой оси. Найти длины отрезков AC и BC и их сумму.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A, B, C;
+    cout << "___A____B____C___" << endl << endl;
+    cout << "Введите координаты точек по порядку на числовой прямой" << endl;
+    cin >> A >> B >> C;
+    cout << endl << "Длина отрезка AC = " << C - A << endl;
+    cout << "Длина отрезка BC = " << C - B << endl;
+    cout << "Сумма длин отрезков AC и BC = " << (C - A) + (C - B) << endl;
+    return 0;
+}
